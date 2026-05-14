c7712d80ab0c12c09e2019753656b0ad2b8fc49e
Deepshika Palepu <150011699+deepshikapalepu20@users.noreply.github.com>
2026-05-15 05:05:36 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+FROM python:3.10-slim
+
+WORKDIR /app
+
+COPY . .
+
+RUN pip install -r requirements.txt
+
+=======
+FROM python:3.10-slim
+
+WORKDIR /app
+
+COPY . .
+
+RUN pip install -r requirements.txt
+
+>>>>>>> 27283fbbfb3921e303e62e06505444af1a18331f
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "CI/CD Pipeline Working!"
+
+if __name__ == "__main__":
+=======
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "CI/CD Pipeline Working!"
+
+if __name__ == "__main__":
+>>>>>>> 27283fbbfb3921e303e62e06505444af1a18331f
+    app.run(host="0.0.0.0", port=5000)
